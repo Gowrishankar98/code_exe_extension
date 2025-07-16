e24b454f8efedf0c2aafd2b59641838c10778f01
Gowri-ShankarK <gowrishankar.kulasekar@archents.com>
2025-07-16 10:59:38 +0530

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,57 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("code_exe.generateCode", () => {
+      const panel = vscode.window.createWebviewPanel(
+        "code_exeChat",
+        "💬 Code_exe Chat",
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+          localResourceRoots: [
+            vscode.Uri.file(path.join(context.extensionPath, "media")),
+          ],
+        }
+      );
+
+      const htmlPath = path.join(context.extensionPath, "src", "panel.html");
+      const htmlContent = fs.readFileSync(htmlPath, "utf8");
+      panel.webview.html = htmlContent;
+
+      panel.webview.onDidReceiveMessage(async (message) => {
+        console.log("📥 Got message from webview:", message);
+        if (message.command === "ask") {
+          console.log("🧠 Got prompt from webview:", message.text);
+
+          try {
+            const response = await fetch("http://127.0.0.1:8000/generate", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ prompt: message.text }),
+            });
+
+            const data = (await response.json()) as { code: string };
+            if (typeof data === "object" && "code" in data) {
+              panel.webview.postMessage({
+                command: "answer",
+                text: (data as any).code,
+              });
+              console.log("✅ Backend responded:", data);
+            }
+          } catch (err) {
+            console.error("❌ Fetch error:");
+            panel.webview.postMessage({
+              command: "answer",
+              text: "Failed to connect to backend: ",
+            });
+          }
+        }
+      });
+    })
+  );
+}
+
+export function deactivate() {}
